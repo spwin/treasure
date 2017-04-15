3712f800443b4578c631cc83c703d798c19eac88
daridang <admin@darida.com>
2017-04-15 19:52:21 +0100

    pass post as refresh token

--- app/Http/Controllers/ApiLoginController.php
+++ app/Http/Controllers/ApiLoginController.php
@@ -30,10 +30,10 @@ class ApiLoginController extends Controller
     public function refresh(Request $request){
         //$refreshToken = $request->cookie(self::REFRESH_TOKEN);
         $refreshToken = $request->get('refresh_token');
-
-        return $this->proxy('refresh_token', [
+        return json_encode(['token' => $refreshToken]);
+        /*return $this->proxy('refresh_token', [
             'refresh_token' => $refreshToken
-        ]);
+        ]);*/
     }
 
     public function logout(){
